46142d5c7b58487b2f836947bce329ccb39445b9
Edgar Davtyan <edo99davtyan@gmail.com>
2017-09-17 19:13:45 +0400

    The 2nd JS homework

---
+++ homework3.js
@@ -0,0 +1,54 @@
+const volumeOfCuboid = function(width, length, height) {
+	const calculation = width * length * height;
+	return calculation;
+};
+
+const noInput = function() {
+	return "Hi, I am a useless fuction. Guess what? I have no purpose!!!";
+};
+
+const LifePurpose = function(YourThoughts) {
+	YourThoughts = "Potato";
+};
+
+const fullName = function(firstName, lastName) {
+	return firstName + " " + lastName
+};
+
+
+const LongerGuy = function(string1, string2) {
+	if(string1.length > string2.length){
+		return string1;
+	} else {
+		return string2;
+	}
+};
+
+const theLongestGuy = function(string1, string2, string3) {
+	return LongerGuy(LongerGuy(string1, string2), string3);
+};
+ 
+
+const zeroOneOrMinusOne = function(num1, num2) {
+	if(num1 > num2){
+		return 1
+	} else if num1 < num2 {
+		return -1
+	} else {
+		return 0
+	}
+};
+
+
+const ifTrue = function(input1, input2) {
+	if !!input1 {
+		return input1
+	} else if !!input2 {
+		return input2 
+	}
+
+};
+
+const firstTruethy = function(input1, input2, input3) {
+	return ifTrue(ifTrue(input1, input2), input3)
+};

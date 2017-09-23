fce97c1cb94a14053be1f7267d3d3d5af62d67d3
Edgar Davtyan <edo99davtyan@gmail.com>
2017-09-23 14:07:10 +0400

    final fixes

--- homework3.js
+++ homework3.js
@@ -9,16 +9,15 @@ const noInput = function() {
 
 const LifePurpose = function(YourThoughts) {
 	YourThoughts = "Potato.";
-	console.log(YourThoughts);
 };
 
 const fullName = function(firstName, lastName) {
-	return firstName + " " + lastName
+	return firstName + " " + lastName;
 };
 
 
 const LongerGuy = function(string1, string2) {
-	if(string1.length > string2.length){
+	if(string1.length > string2.length) {
 		return string1;
 	} else if string2.length > string1.length {
 		return string2;
@@ -33,9 +32,9 @@ const theLongestGuy = function(string1, string2, string3) {
  
 
 const zeroOneOrMinusOne = function(num1, num2) {
-	if(num1 > num2){
+	if(num1 > num2) {
 		return 1;
-	} else if num1 < num2 {
+	} else if (num1 < num2) {
 		return -1;
 	} else {
 		return 0;
@@ -43,14 +42,14 @@ const zeroOneOrMinusOne = function(num1, num2) {
 };
 
 const ifTrue = function(input1, input2) {
-	if !!input1 {
+	if (!!input1) {
 		return input1;
-	} else if !!input2 {
+	} else if (!!input2) {
 		return input2;
 	}
 
 };
 
 const firstTruethy = function(input1, input2, input3) {
-	return ifTrue(ifTrue(input1, input2), input3)
+	return ifTrue(ifTrue(input1, input2), input3);
 };
